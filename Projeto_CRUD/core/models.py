ef1ae5f3b73b7e05f598d6702daa060d297092e6
mateus do skate <h.tamura@aluno.ifsp.edu.br>
2026-08-13 20:12:25 -0300

    a

--- Projeto_CRUD/core/models.py
+++ Projeto_CRUD/core/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 class Categoria(models.Model):
-    categoria = models.CharField(max_length=150)
+    categoria = models.CharField(max_length=150)    
+    
+    def __str__(self):
+        return self.categoria
 
 class Modelo(models.Model):
     modelo = models.CharField(max_length=150)
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+        
+    def __str__(self):
+        return self.modelo
 
 class Roupa(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
